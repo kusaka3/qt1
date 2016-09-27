6616de836e8986a83144bf47a15878c8556fde6f
kusaka <objectve874@gmail.com>
2016-09-27 14:58:26 +0900

    高DPI環境への対応を追加

--- Program.cs
+++ Program.cs
@@ -14,9 +14,13 @@ namespace qt1
         [STAThread]
         static void Main()
         {
+            SetProcessDPIAware(); //高DPIのサポート
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public extern static IntPtr SetProcessDPIAware();
     }
 }
